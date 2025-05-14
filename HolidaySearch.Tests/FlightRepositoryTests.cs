2a1ac794d7a5bc9af261efbd861de6fa6f00db24
Travis CI <enquiry@streetsupport.net>
2025-05-14 21:24:02 +0100

    - Added Mock Json files - Added passing test for valid Jason file with valid data

--- HolidaySearch.Tests/FlightRepositoryTests.cs
+++ HolidaySearch.Tests/FlightRepositoryTests.cs
@@ -7,7 +7,8 @@ namespace HolidaySearch.Tests.Repository
     [TestFixture]
     public class FlightRepositoryTests
     {        
-        private string _WrongFilePath = "..//..//..//MockJsonData//NotHere.json";        
+        private string _WrongFilePath = "..//..//..//MockJsonData//NotHere.json";
+        private string _MockDataFilePath = "..//..//..//MockJsonData//FlightData.json";
 
         [SetUp]
         public void SetUp()
@@ -21,6 +22,18 @@ namespace HolidaySearch.Tests.Repository
             var flightRepo = () => new FlightRepository(_WrongFilePath);
             // Assert
             flightRepo.Should().Throw<FileNotFoundException>();
+        }
+
+        [Test]
+        public void Given_A_File_With_ValidData_Then_Returned_RawData_Should_Be_Populated_With_12_Records()
+        {
+            // Arrange
+            var flightRepo = new FlightRepository(_MockDataFilePath);
+            // Act
+            var flightData = flightRepo.RawData;
+            // Assert
+            flightData.Should().NotBeNullOrEmpty();
+            flightData.Should().HaveCount(12);
         }
 
     }
