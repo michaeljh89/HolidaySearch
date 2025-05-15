b6a16f40e25fc3fbb238908cb2c469156b2a6919
Travis CI <enquiry@streetsupport.net>
2025-05-15 20:36:26 +0100

    - added check on searchCrieria data to throw execption for passing test

--- HolidaySearch/SearchHolidays.cs
+++ HolidaySearch/SearchHolidays.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using HolidaySearch.Models;
 
 namespace HolidaySearch
@@ -17,6 +19,13 @@ namespace HolidaySearch
             {
                 throw new ArgumentNullException(nameof(searchCriteria), "Search criteria cannot be null");
             }
+
+            if (searchCriteria.DepartingFrom.Length != 3 || searchCriteria.TravelingTo.Length != 3
+                || searchCriteria.Duration <= 0 )
+            {
+                throw new InvalidDataException("Invalid search criteria");
+            }
+
             List<HolidayPackageResult> holidayPackageResults = new List<HolidayPackageResult>();
             return holidayPackageResults;
         }
