467caa206ac185a6209063cd089b6bd2278c0ad6
Travis CI <enquiry@streetsupport.net>
2025-05-15 20:03:01 +0100

    - added model for holidayPackageResult

---
+++ HolidaySearch/Models/HolidayPackageResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Models
+{
+    internal class HolidayPackageResult
+    {
+        public decimal TotalPrice { get; set; }
+        public Flight Flight { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
